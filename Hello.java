6965685379e076f86ca132eb1048812806883150
mohammede2 <112145428+mohammede2@users.noreply.github.com>
2022-08-25 23:17:47 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/*************************************************************************************
+* Hello.java
+**************************************************************************************
+* Eman Mohammed
+* 24 August 2022
+* 225-001
+*************************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Eman Mohammed>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Java/create and run a java program>");
+		System.out.println("<24 August 2022>");
+		System.out.println("<225-001>");
+		System.out.println();
+
+	}
+}
+                                         
+
